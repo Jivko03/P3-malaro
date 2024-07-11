b3bd016af622ceec20d21c3fa097cf84fb03c4a0
Jivko03 <47494058+Jivko03@users.noreply.github.com>
2024-07-11 12:29:49 +0300

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -3,7 +3,7 @@ cd /var/www/html || exit
 git remote set-url origin git@github.com:Jivko03/P3-malaro.git
 if [ -d "/var/www/html" ];
 then
-cd ~/html/P3-malaro
+cd /var/www/html/P3-malaro
 git pull origin main 
 else
 git clone git@github.com:Jivko03/P3-malaro.git
